a5eac1a0db8749869945843afb24d8b3637703ec
kavtara <kavtara123@gmail.com>
2019-05-10 16:26:08 +0400

    Overplay update

--- assets/JS/Jscript.js
+++ assets/JS/Jscript.js
@@ -5,15 +5,34 @@ e.id = "onclick-button";
 document.getElementById("onclick-button").text = "დამატებულია";
 }
 
-function onClickMenu(){
-	document.getElementById("book2").class.toggle("change");
+// function onClickMenu(){
+	// document.getElementById("book2").class.toggle("change");
 	//document.getElementById("nav").classList.toggle("change");
 	//document.getElementById("menu-bg").classList.toggle("change");
 
+
+
+function hide(){
+	document.getElementById("input").innerHTML= "Thank you! Your message has been sent successfully.";	
+	document.getElementById("green_image").src = "assets/images/pts2.png";
+
+
+}
+
+ function test () {
+ 	document.getElementById("book2").id.toggle("menu"); 
 }
 
 
+
+// function myFunction() {
+//    var element = document.getElementById("book2");
+//    element.class.toggle("burger");
+//    document.getElementById("overlay").style.display = "block";
+// }
+
 function on() {
+
   document.getElementById("overlay").style.display = "block";
 }
 
