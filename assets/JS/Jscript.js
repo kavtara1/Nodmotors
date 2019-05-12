2884b7fa6bee9ed97cb7e3479139f6d559c3459c
kavtara1 <40422017+kavtara1@users.noreply.github.com>
2019-05-13 01:04:01 +0400

    Add files via upload

--- assets/JS/Jscript.js
+++ assets/JS/Jscript.js
@@ -35,10 +35,10 @@ function hide(){
 
 function onof() {
   var x = document.getElementById("overlay");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
+  if (x.style.display === "block" )  {
     x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
 }
 
