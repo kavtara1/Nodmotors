faaafc772246ecf9c013cf927c49c8a9b139393e
kavtara <kavtara123>
2019-05-13 03:17:18 +0400

    dradadada

--- assets/JS/Jscript.js
+++ assets/JS/Jscript.js
@@ -37,8 +37,10 @@ function onof() {
   var x = document.getElementById("overlay");
   if (x.style.display === "block" )  {
     x.style.display = "none";
+    document.getElementById("menu").classList.toggle("change");
   } else {
     x.style.display = "block";
+    document.getElementById("menu").classList.toggle("change");
   }
 }
 
